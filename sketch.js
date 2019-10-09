e6bb82c92338c7316937fa0876be7cd60df0b3bb
heyjunesim <55528344+heyjunesim@users.noreply.github.com>
2019-10-09 21:57:44 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,67 +1,62 @@
 let img;
 function preload() {
   img = loadImage('lightning.png');
+  rainSound = loadSound('rain_inside_house.mp3');
 }
-  
-var max_rainDrops = 1000;
+
+var max_rainDrops = 200;
 var rainDrops = [];
 var rainSound;
 
-function rainDrop(x, y, vy, sz, c) {
+function rainDrop(x,y,vy,sz,c) {
   this.x = x;
   this.y = y;
   this.vy = vy;
   this.sz = sz;
   this.c = c;
-
+  
   this.move = function() {
-    this.y += this.vy; 
+    this.y += this.vy;
     if (this.y>windowHeight) this.y = 0;
-
+    
     if (mouseIsPressed) {
       var xdif = abs(this.x-mouseX);
       if (xdif < 100 + random(-100,100)) {
-        if ( (this.y- mouseY) > random(-100,100)) {
+        if ( (this.y - mouseY) > random(-100,100)) {
           this.y=0;
         }
       }
-    }
-    if (mousePressed) {
       image(img,mouseX,mouseY);
     }
   }
-
-  this.render = function() { 
-    noStroke();
+  
+  this.render = function() {
+    noStorke();
     fill(this.c);
-    ellipse(this.x, this.y, 2, this.sz);
+    ellipse(this.x,this.y,2,this.sz);
   }
 }
 
-//function preload() {
-  //rainSound = loadSound('rain_inside_house.mp3');
-//}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
+  createCanvas(windowWidth,windowHeight);
+  
   var i;
   for (i=0; i<max_rainDrops; i++) {
     rainDrops[i] = new rainDrop(
-      random(0, windowWidth), random(0, windowHeight), 
-      random(30, 100), 
-      random(30, 100), color(random(100, 255)) );
+      random(0,windowWidth), random(0,windowHeight),
+      random(30,100), random(30,100), color(random(100,255)) );
   }
   
-  //rainSound.loop();
+  rainSound.loop();
 }
 
 function draw() {
   background(0);
-
+  
   var i;
   for (i=0; i<max_rainDrops; i++) {
     rainDrops[i].move();
     rainDrops[i].render();
   }
-}
+} 
+  
